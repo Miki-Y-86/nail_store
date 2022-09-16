9962dfcbc293d1ea72c86058ae7af69dedf63f19
Miki-Y-86 <53904005+Miki-Y-86@users.noreply.github.com>
2022-09-17 07:57:59 +0900

    Create tranSale.php

---
+++ tranSale.php
@@ -0,0 +1,38 @@
+<?php
+$siteTitle = '販売履歴';
+require('head.php');
+?>
+
+<body class="page-withdraw page-1colum">
+
+<style>
+    .form .btn{
+        float: none;
+    }
+    .form{
+        text-align: center;
+    }
+</style>
+
+<!-- メニュー -->
+<?php
+require('header.php');
+?>
+
+<!-- メインコンテンツ -->
+<div id="contents" class="site-width">
+    <!-- Main -->
+    <section id="main" >
+        <h1 class="page-title">販売履歴</h1>
+
+        <div class="box">
+            商品はありません
+        </div>
+
+    </section>
+</div>
+<a href="mypage.php">&lt; マイページに戻る</a>
+<!-- footer -->
+<?php
+require('footer.php');
+?>
